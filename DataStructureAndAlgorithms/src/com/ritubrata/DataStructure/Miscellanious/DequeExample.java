38dd60affda5569ca18c094094351e3f6c359b7b
Ritubrata Chatterjee <ritubrata91@gmail.com>
2021-07-03 19:28:28 +0530

    Added features related to Priority and De queue.

--- DataStructureAndAlgorithms/src/com/ritubrata/DataStructure/Miscellanious/DequeExample.java
+++ DataStructureAndAlgorithms/src/com/ritubrata/DataStructure/Miscellanious/DequeExample.java
@@ -8,21 +8,26 @@ public class DequeExample {
 		final Deque<String> deque = new LinkedList<String>();
 		// Add at the last
 		deque.add("Element 1 (Tail)");
+		deque.addLast("Element 3 (Tail)");
+		deque.offer("Element 5 (Tail)");
+
 		// Add at the first
 		deque.addFirst("Element 2 (Head)");
-		// Add at the last
-		deque.addLast("Element 3 (Tail)");
-		// Add at the first
 		deque.push("Element 4 (Head)");
-		// Add at the last
-		deque.offer("Element 5 (Tail)");
-		// Add at the first
 		deque.offerFirst("Element 6 (Head)");
 
 		System.out.println(deque + "\n");
 
+		System.out.println(deque.peek());  //Retrieves, but does not remove the head of the queue  or returns null if this deque is empty.
+		System.out.println(deque.poll()); //Retrieves and remove the head of the queue  or returns null if this deque is empty.
+		System.out.println(deque.pop()); //Removes and returns the first element of this deque.
+
+		deque.push("Element 7");  //Pushes an element at the head of this deque if it is possible to do so immediately without violating capacity
+		//restrictions, throwing an IllegalStateException if no space is currently available.
+
 		deque.removeFirst();
 		deque.removeLast();
+
 		System.out.println("Deque after removing first and last: "+ deque);
 	}
 }
--- DataStructureAndAlgorithms/src/com/ritubrata/DataStructure/Miscellanious/PriorityQueueExample.java
+++ DataStructureAndAlgorithms/src/com/ritubrata/DataStructure/Miscellanious/PriorityQueueExample.java
@@ -7,23 +7,32 @@ public class PriorityQueueExample {
 
 	public static void main(final String args[]){
 		final PriorityQueue<String> queue=new PriorityQueue<String>();
+		/* It is only ordered/sorted in the sense that the first element is the least.
+		In other word, it only cares about what is in the front of the queue,
+		the rest are "ordered" when needed. */
 		queue.add("Amit");
 		queue.add("Vijay");
 		queue.add("Karan");
 		queue.add("Jai");
 		queue.add("Rahul");
-		System.out.println("head:"+queue.element());
-		System.out.println("head:"+queue.peek());
+
+		System.out.println("head:"+queue.element()); //for retrieving head element, not removing ... it throws exception if queue is empty.
+		System.out.println("*****************************************************");
+		System.out.println("head:"+queue.peek());   //for retrieving head element, not removing... it returns null if queue is empty.
+		System.out.println("*****************************************************");
+
 		System.out.println("iterating the queue elements:");
 		final Iterator<String> itr=queue.iterator();
 		while(itr.hasNext()){
 			System.out.println(itr.next());
 		}
-		queue.remove();
-		queue.poll();
+		System.out.println("*****************************************************");
+		queue.remove();  //for retrieving  and removing head element... it returns exception if queue is empty.
+		queue.poll(); //for retrieving  and removing head element... it returns null if queue is empty.
 		System.out.println("after removing two elements:");
 		final Iterator<String> itr2=queue.iterator();
 		while(itr2.hasNext()){
+			System.out.println("*****************************************************");
 			System.out.println(itr2.next());
 		}
 	}
